daf38720e1765a37e83fbe8a0f24b80613f8b9e2
viclem <viclem1614@gmail.com>
2024-09-25 18:08:13 +0200

    modif fichier config pour cors

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -16,6 +16,8 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 // How many rounds should bcrypt run the salt (default - 10 rounds)
 const saltRounds = 10;
 
+
+
 // POST /auth/signup  - Creates a new user in the database
 router.post("/signup", (req, res, next) => {
   const { email, password } = req.body;
